37d7f13c918a98657829030b136c9f6c8a08c25b
aka-nick <bechalrang@gmail.com>
2023-04-15 11:01:39 +0900

    path variable 적용

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+  const { id } = useParams();
+
   return (
     <div>
       <h1>Diary</h1>
-      <p>이곳은 Diary입니다.</p>
+      <p>이곳은 {id}번 Diary입니다.</p>
     </div>
   );
 };
